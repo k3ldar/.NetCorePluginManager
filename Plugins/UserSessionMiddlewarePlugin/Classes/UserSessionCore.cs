05f76c7c9ef547b42fba6b897f87c26bef2efbdd
k3ldar <s1cart3r@gmail.com>
2018-09-29 09:47:43 +0100

    Initially added user session middleware plugin

---
+++ Plugins/UserSessionMiddlewarePlugin/Classes/UserSessionCore.cs
@@ -0,0 +1,175 @@
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  .Net Core Plugin Manager is distributed under the GNU General Public License version 3 and  
+ *  is also available under alternative licenses negotiated directly with Simon Carter.  
+ *  If you obtained Service Manager under the GPL, then the GPL applies to all loadable 
+ *  Service Manager modules used on your system as well. The GPL (version 3) is 
+ *  available at https://opensource.org/licenses/GPL-3.0
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  The Original Code was created by Simon Carter (s1cart3r@gmail.com)
+ *
+ *  Copyright (c) 2018 Simon Carter.  All Rights Reserved.
+ *
+ *  Product:  UserSessionMiddleware.Plugin
+ *  
+ *  File: UserSessionCore.cs
+ *
+ *  Purpose:  
+ *
+ *  Date        Name                Reason
+ *  29/09/2018  Simon Carter        Initially Created
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+using System;
+
+using Microsoft.AspNetCore.Http;
+
+using Shared.Classes;
+
+namespace UserSessionMiddleware.Plugin
+{
+    public class UserSessionCore : UserSession
+    {
+        #region Constructor
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public UserSessionCore()
+            : base()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="created"></param>
+        /// <param name="sessionID"></param>
+        /// <param name="userAgent"></param>
+        /// <param name="initialReferer"></param>
+        /// <param name="ipAddress"></param>
+        /// <param name="hostName"></param>
+        /// <param name="isMobile"></param>
+        /// <param name="isBrowserMobile"></param>
+        /// <param name="mobileRedirect"></param>
+        /// <param name="referralType"></param>
+        /// <param name="bounced"></param>
+        /// <param name="isBot"></param>
+        /// <param name="mobileManufacturer"></param>
+        /// <param name="mobileModel"></param>
+        /// <param name="userID"></param>
+        /// <param name="screenWidth"></param>
+        /// <param name="screenHeight"></param>
+        /// <param name="saleCurrency"></param>
+        /// <param name="saleAmount"></param>
+        public UserSessionCore(long id, DateTime created, string sessionID, string userAgent, string initialReferer,
+            string ipAddress, string hostName, bool isMobile, bool isBrowserMobile, bool mobileRedirect,
+            ReferalType referralType, bool bounced, bool isBot, string mobileManufacturer, string mobileModel,
+            long userID, int screenWidth, int screenHeight, string saleCurrency, decimal saleAmount)
+            : base(id, created, sessionID, userAgent, initialReferer, ipAddress, hostName, isMobile,
+                  isBrowserMobile, mobileRedirect, referralType, bounced, isBot, mobileManufacturer, mobileModel,
+                  userID, screenWidth, screenHeight, saleCurrency, saleAmount)
+        {
+        }
+
+        /// <summary>
+        /// Constructor
+        /// 
+        /// Allows passing of user defined object
+        /// </summary>
+        /// <param name="context">HTTP Context </param>
+        /// <param name="tag">User defined object</param>
+        public UserSessionCore(in HttpContext context, in string sessionId, in object tag)
+            : this(context, sessionId)
+        {
+            Tag = tag;
+            InternalSessionID = -1;
+        }
+
+        /// <summary>
+        /// Constructor
+        /// 
+        /// Allows passing of user name and email
+        /// </summary>
+        /// <param name="Session">Current User Session</param>
+        /// <param name="Request">Current Web Request</param>
+        /// <param name="userName">Current user's name</param>
+        /// <param name="userEmail">Current user's email address</param>
+        /// <param name="userID">Current user's unique id</param>
+        public UserSessionCore(in HttpContext context, in string sessionId, in string userName, in string userEmail,
+            Int64 userID)
+            : this(context, sessionId)
+        {
+            UserName = userName;
+            UserEmail = userEmail;
+            UserID = userID;
+        }
+
+        /// <summary>
+        /// Constructor
+        /// 
+        /// Standard constructor
+        /// </summary>
+        /// <param name="Session">Current User Session</param>
+        /// <param name="Request">Current Web Request</param>
+        public UserSessionCore(HttpContext context, string sessionId)
+            : base()
+        {
+            Created = DateTime.Now;
+            CurrentSale = 0.00m;
+            CurrentSaleCurrency = String.Empty;
+            Tag = null;
+
+            SessionID = sessionId;
+            IPAddress = context.Connection.RemoteIpAddress.ToString();
+
+            if (IPAddress == "::1")
+                IPAddress = "127.0.0.1";
+
+            HostName = context.Request.Host.Host;
+            UserAgent = context.Request.Headers["User-Agent"].ToString();
+            IsMobileDevice = CheckIfMobileDevice(UserAgent);
+            IsBrowserMobile = false;// Request.Browser.IsMobileDevice;
+
+            MobileRedirect = IsMobileDevice | IsBrowserMobile;
+
+            MobileManufacturer = "";// Request.Browser.MobileDeviceManufacturer;
+            MobileModel = ""; // Request.Browser.MobileDeviceModel;
+            ScreenHeight = 10;// Request.Browser.ScreenPixelsHeight;
+            ScreenWidth = 10;// Request.Browser.ScreenPixelsWidth;
+
+            try
+            {
+                string referrer = context.Request.Headers["Referer"].ToString();
+                if (String.IsNullOrEmpty(referrer))
+                    Referal = ReferalType.Unknown;
+                else
+                    InitialReferrer = referrer ?? String.Empty;
+            }
+            catch (Exception err)
+            {
+                if (!err.Message.Contains("The hostname could not be parsed"))
+                    throw;
+            }
+
+            CountryCode = String.Empty;
+
+            UserName = String.Empty;
+            UserEmail = String.Empty;
+            UserID = -1;
+
+            UserSessionManager.UpdateSession(this);
+
+            SaveStatus = SaveStatus.Pending;
+            PageSaveStatus = SaveStatus.Saved;
+            InternalSessionID = Int64.MinValue;
+        }
+
+        #endregion Constructor
+    }
+}
---
+++ Plugins/UserSessionMiddlewarePlugin/Classes/UserSessionMiddleware.cs
@@ -0,0 +1,188 @@
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  .Net Core Plugin Manager is distributed under the GNU General Public License version 3 and  
+ *  is also available under alternative licenses negotiated directly with Simon Carter.  
+ *  If you obtained Service Manager under the GPL, then the GPL applies to all loadable 
+ *  Service Manager modules used on your system as well. The GPL (version 3) is 
+ *  available at https://opensource.org/licenses/GPL-3.0
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  The Original Code was created by Simon Carter (s1cart3r@gmail.com)
+ *
+ *  Copyright (c) 2018 Simon Carter.  All Rights Reserved.
+ *
+ *  Product:  UserSessionMiddleware.Plugin
+ *  
+ *  File: UserSessionMiddleware.cs
+ *
+ *  Purpose:  
+ *
+ *  Date        Name                Reason
+ *  29/09/2018  Simon Carter        Initially Created
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+using System;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+using Shared.Classes;
+
+namespace UserSessionMiddleware.Plugin
+{
+    public sealed class UserSessionMiddleware
+    {
+        #region Private Members
+
+        private static int _cookieID;
+
+        private readonly RequestDelegate _next;
+        private readonly string _cookieName = "user_session";
+        private readonly string _cookieEncryptionKey = "Dfklaosre;lnfsdl;jlfaeu;dkkfcaskxcd3jf";
+        private readonly string _staticFileExtension = ".ico;.css;.js;.svg;.jpg;.jpeg;.gif;.png;.eot;";
+
+        #endregion Private Members
+
+        #region Constructors
+
+        public UserSessionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+
+            UserSessionSettings settings = GetUserSessionSettings();
+
+            if (!String.IsNullOrWhiteSpace(settings.CookieName))
+                _cookieName = settings.CookieName;
+
+            if (!String.IsNullOrWhiteSpace(settings.EncryptionKey))
+                _cookieEncryptionKey = settings.EncryptionKey;
+
+            if (!String.IsNullOrWhiteSpace(settings.StaticFileExtensions))
+                _staticFileExtension = settings.StaticFileExtensions;
+        }
+
+        #endregion Constructors
+
+        #region Public Methods
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                // if it's a static file, don't add user session data to the context
+                if (_staticFileExtension.Contains($"{context.Request.Path.ToString().ToLower()};"))
+                    return;
+
+                string cookieSessionID;
+                CookieOptions options = new CookieOptions()
+                {
+                    HttpOnly = false
+                };
+
+                if (context.Request.Cookies.ContainsKey(_cookieName))
+                {
+                    cookieSessionID = Shared.Utilities.Decrypt(context.Request.Cookies[_cookieName], _cookieEncryptionKey);
+                    context.Response.Cookies.Append(_cookieName,
+                        Shared.Utilities.Encrypt(cookieSessionID, _cookieEncryptionKey), options);
+                }
+                else
+                {
+                    cookieSessionID = GetNextID(context);
+                    context.Response.Cookies.Append(_cookieName,
+                        Shared.Utilities.Encrypt(cookieSessionID, _cookieEncryptionKey), options);
+                }
+
+                CacheItem currentSession = UserSessionManager.UserSessions.Get(cookieSessionID);
+                UserSession userSession = null;
+
+                if (currentSession != null)
+                {
+                    userSession = (UserSession)currentSession.Value;
+                }
+                else
+                {
+                    userSession = GetUserSession(context, cookieSessionID);
+                }
+
+                string referrer = context.Request.Headers["Referer"];
+                userSession.PageView(GetAbsoluteUri(context).ToString(), referrer ?? String.Empty, false);
+
+                context.Items.Add("UserSession", userSession);
+            }
+            catch (Exception error)
+            {
+                Initialisation.GetLogger.AddToLog(error, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+            finally
+            {
+                await _next(context);
+            }
+        }
+
+        #endregion Public Methods
+
+        #region Private Methods
+
+        private string GetNextID(HttpContext context)
+        {
+            return ($"SN{DateTime.Now.ToFileTimeUtc()}{_cookieID++}");
+        }
+
+        private Uri GetAbsoluteUri(HttpContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            UriBuilder uriBuilder = new UriBuilder(context.Request.Scheme, context.Request.Host.Host)
+            {
+                Path = context.Request.Path.ToString(),
+                Query = context.Request.QueryString.ToString()
+            };
+
+            return (uriBuilder.Uri);
+        }
+
+        private UserSession GetUserSession(in HttpContext context, in string sessionId)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+
+            if (String.IsNullOrEmpty(sessionId))
+                throw new ArgumentNullException(nameof(sessionId));
+
+            try
+            {
+                UserSession Result = new UserSessionCore(context, sessionId);
+
+                UserSessionManager.Add(Result);
+
+                UserSessionManager.Instance.InitialiseSession(Result);
+
+                return (Result);
+            }
+            catch (Exception err)
+            {
+                Initialisation.GetLogger.AddToLog(err);
+            }
+
+            return (null);
+        }
+
+        private UserSessionSettings GetUserSessionSettings()
+        {
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            IConfigurationBuilder configBuilder = builder.SetBasePath(System.IO.Directory.GetCurrentDirectory());
+            configBuilder.AddJsonFile("appsettings.json");
+            IConfigurationRoot config = builder.Build();
+            UserSessionSettings Result = new UserSessionSettings();
+            config.GetSection("UserSessionConfiguration").Bind(Result);
+
+            return (Result);
+        }
+
+        #endregion Private Methods
+    }
+}
