7f0461bb344247de59a65f8ff89b9674c5bbaccb
k3ldar <s1cart3r@gmail.com>
2018-10-04 19:49:58 +0100

    Add GeoIp Data Service and User Session service interfaces to SharedPluginFeatures

---
+++ Plugins/SharedPluginFeatures/Interfaces/IUserSessionService.cs
@@ -0,0 +1,42 @@
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  .Net Core Plugin Manager is distributed under the GNU General Public License version 3 and  
+ *  is also available under alternative licenses negotiated directly with Simon Carter.  
+ *  If you obtained Service Manager under the GPL, then the GPL applies to all loadable 
+ *  Service Manager modules used on your system as well. The GPL (version 3) is 
+ *  available at https://opensource.org/licenses/GPL-3.0
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  The Original Code was created by Simon Carter (s1cart3r@gmail.com)
+ *
+ *  Copyright (c) 2018 Simon Carter.  All Rights Reserved.
+ *
+ *  Product:  SharedPluginFeatures
+ *  
+ *  File: IUserSessionService.cs
+ *
+ *  Purpose:  Provides interface for Managing user sessions
+ *
+ *  Date        Name                Reason
+ *  04/10/2018  Simon Carter        Initially Created
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+using Shared.Classes;
+
+namespace SharedPluginFeatures
+{
+    public interface IUserSessionService
+    {
+        void Save(in UserSession userSession);
+
+        void Retrieve(in string userSessionId, ref UserSession userSession);
+
+        void Closing(in UserSession userSession);
+
+        void SavePage(in UserSession pageView);
+
+        void Created(in UserSession userSession);
+    }
+}
