9f6c8b60740fe399f0014b54efb1c481c58200e3
k3ldar <s1cart3r@gmail.com>
2018-10-06 11:02:12 +0100

    Add Spider attribute to shared features

---
+++ Plugins/SharedPluginFeatures/Attributes/SpiderAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SharedPluginFeatures
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public sealed class SpiderAttribute : Attribute
+    {
+        #region Constructors
+
+        public SpiderAttribute()
+            : this ("*", false)
+        {
+
+        }
+
+        public SpiderAttribute(in string userAgent, in bool allowSpider)
+            : this (userAgent, allowSpider, String.Empty)
+        {
+
+        }
+
+        public SpiderAttribute(in string userAgent, in bool allowSpider, in string comment)
+        {
+            if (String.IsNullOrEmpty(userAgent))
+                throw new ArgumentNullException(nameof(userAgent));
+
+            UserAgent = userAgent;
+            AllowSpider = allowSpider;
+            Comment = comment;
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public bool AllowSpider { get; private set; }
+
+        public string UserAgent { get; private set; }
+
+        public string Comment { get; private set; }
+
+        #endregion Properties
+    }
+}
