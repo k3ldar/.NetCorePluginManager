4d047096d5bf5bf0d34e3884825511be00579059
k3ldar <s1cart3r@gmail.com>
2018-10-13 20:55:26 +0100

    New future interfaces

---
+++ Plugins/SharedPluginFeatures/Attributes/RequestManagerMiddlewareExtender.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RequestManager.Plugin
+{
+    class RequestManagerMiddlewareExtender
+    {
+    }
+}
---
+++ Plugins/SharedPluginFeatures/Interfaces/IRouteDataService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace SharedPluginFeatures
+{
+    public interface IRouteDataService
+    {
+        string GetRouteFromClass(Type type, IActionDescriptorCollectionProvider routeProvider);
+
+        string GetRouteFromMethod(in MethodInfo method, in IActionDescriptorCollectionProvider routeProvider);
+    }
+}
