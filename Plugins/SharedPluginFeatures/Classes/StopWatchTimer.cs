87d314b2d97a0f32695eeb886eabcaec2488a4c7
k3ldar <s1cart3r@gmail.com>
2018-11-11 07:28:45 +0000

    Timer Class for timing min/max/average for a routine

--- Plugins/SharedPluginFeatures/Classes/StopWatchTimer.cs
+++ Plugins/SharedPluginFeatures/Classes/StopWatchTimer.cs
@@ -1,5 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  .Net Core Plugin Manager is distributed under the GNU General Public License version 3 and  
+ *  is also available under alternative licenses negotiated directly with Simon Carter.  
+ *  If you obtained Service Manager under the GPL, then the GPL applies to all loadable 
+ *  Service Manager modules used on your system as well. The GPL (version 3) is 
+ *  available at https://opensource.org/licenses/GPL-3.0
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  The Original Code was created by Simon Carter (s1cart3r@gmail.com)
+ *
+ *  Copyright (c) 2018 Simon Carter.  All Rights Reserved.
+ *
+ *  Product:  SharedPluginFeatures
+ *  
+ *  File: StopWatchTimer.cs
+ *
+ *  Purpose:  Timer for perfomance counting
+ *
+ *  Date        Name                Reason
+ *  10/11/2018  Simon Carter        Initially Created
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+using System;
 using System.Diagnostics;
 
 namespace SharedPluginFeatures
