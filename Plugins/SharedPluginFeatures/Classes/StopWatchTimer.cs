ddb46d100fb68eb5a21d8f386d37dcabc21aa75f
k3ldar <s1cart3r@gmail.com>
2018-11-11 07:25:18 +0000

    Timer Class for timing min/max/average for a routine

---
+++ Plugins/SharedPluginFeatures/Classes/StopWatchTimer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace SharedPluginFeatures
+{
+    public struct StopWatchTimer : IDisposable
+    {
+        #region Private Members
+
+        private Stopwatch _stopwatch;
+        private Timings _timings;
+
+        #endregion Private Members
+
+        #region Constructors
+
+        public StopWatchTimer(in Timings timings)
+        {
+            _timings = timings ?? throw new ArgumentNullException(nameof(timings));
+            _stopwatch = new Stopwatch();
+            _stopwatch.Start();
+        }
+
+        #endregion Constructors
+
+        #region Static Methods
+
+        public static StopWatchTimer Initialise(in Timings timings)
+        {
+            return (new StopWatchTimer(timings));
+        }
+
+        #endregion Static Methods
+
+        #region IDisposable Methods
+
+        public void Dispose()
+        {
+            _stopwatch.Stop();
+            _timings.Increment(_stopwatch);
+        }
+
+        #endregion IDisposable Methods
+    }
+}
---
+++ Plugins/SharedPluginFeatures/Classes/Timings.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace SharedPluginFeatures
+{
+    public sealed class Timings
+    {
+        #region Private Members
+
+        private long _fastest;
+        private long _slowest;
+        private readonly object _lockObject;
+
+        #endregion Private Members
+
+        #region Constructors
+
+        public Timings()
+        {
+            _lockObject = new object();
+            _fastest = long.MaxValue;
+            _slowest = long.MinValue;
+        }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public uint Requests { get; private set; }
+
+        public long Slowest
+        {
+            get
+            {
+                if (_slowest == long.MaxValue)
+                    return(0);
+
+                return (_slowest);
+            }
+        }
+
+        public long Fastest
+        {
+            get
+            {
+                return (_fastest);
+            }
+        }
+
+        public decimal Average { get; private set; }
+
+        public long Total { get; private set; }
+
+        #endregion Properties
+
+        #region Public Methods
+
+        public void Increment(in Stopwatch stopWatch)
+        {
+            Increment(stopWatch.ElapsedMilliseconds);
+        }
+
+        public void Increment(in long totalMilliseconds)
+        {
+            lock (_lockObject)
+            {
+                Requests++;
+
+                if (totalMilliseconds < _fastest)
+                    _fastest = totalMilliseconds;
+
+                if (totalMilliseconds > _slowest)
+                    _slowest = totalMilliseconds;
+
+                Total += totalMilliseconds;
+
+                if (Total > 0)
+                    Average = Total / Requests;
+            }
+        }
+
+        #endregion Public Methods
+    }
+}
