6c196c5db3b5316103bd61c63fc1b6fdb1597b54
k3ldar <s1cart3r@gmail.com>
2019-01-12 21:00:02 +0000

    Partial Implementation

--- Plugins/LocalizationPlugin/Classes/StringLocalizerFactory.cs
+++ Plugins/LocalizationPlugin/Classes/StringLocalizerFactory.cs
@@ -1,13 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  .Net Core Plugin Manager is distributed under the GNU General Public License version 3 and  
+ *  is also available under alternative licenses negotiated directly with Simon Carter.  
+ *  If you obtained Service Manager under the GPL, then the GPL applies to all loadable 
+ *  Service Manager modules used on your system as well. The GPL (version 3) is 
+ *  available at https://opensource.org/licenses/GPL-3.0
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  The Original Code was created by Simon Carter (s1cart3r@gmail.com)
+ *
+ *  Copyright (c) 2018 - 2019 Simon Carter.  All Rights Reserved.
+ *
+ *  Product:  Localization.Plugin
+ *  
+ *  File: StringLocalizerFactory.cs
+ *
+ *  Purpose:  
+ *
+ *  Date        Name                Reason
+ *  12/01/2019  Simon Carter        Initially Created
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+using System;
 
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Localization;
-using Microsoft.AspNetCore.Mvc.Razor;
-using Microsoft.Extensions.Options;
 
 using Shared.Classes;
 
