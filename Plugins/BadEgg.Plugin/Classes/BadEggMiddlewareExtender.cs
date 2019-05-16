498718acff9a6daabb9de3bd5f047b29e9a7a680
k3ldar <s1cart3r@gmail.com>
2019-05-16 20:34:30 +0100

    #74 BadEgg Middleware Extender

---
+++ Plugins/BadEgg.Plugin/Classes/BadEggMiddlewareExtender.cs
@@ -0,0 +1,38 @@
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  .Net Core Plugin Manager is distributed under the GNU General Public License version 3 and  
+ *  is also available under alternative licenses negotiated directly with Simon Carter.  
+ *  If you obtained Service Manager under the GPL, then the GPL applies to all loadable 
+ *  Service Manager modules used on your system as well. The GPL (version 3) is 
+ *  available at https://opensource.org/licenses/GPL-3.0
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  The Original Code was created by Simon Carter (s1cart3r@gmail.com)
+ *
+ *  Copyright (c) 2018 - 2019 Simon Carter.  All Rights Reserved.
+ *
+ *  Product:  BadEgg.Plugin
+ *  
+ *  File: BadEggMiddlewareExtender.cs
+ *
+ *  Purpose:  Adds a middleware extender to easily add BadEgg
+ *
+ *  Date        Name                Reason
+ *  16/05/2019  Simon Carter        Initially Created
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+using Microsoft.AspNetCore.Builder;
+
+namespace BadEgg.Plugin
+{
+    public static class BadEggMiddlewareExtender
+    {
+        public static IApplicationBuilder UseBadEgg(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<BadEggMiddleware>();
+        }
+    }
+}
