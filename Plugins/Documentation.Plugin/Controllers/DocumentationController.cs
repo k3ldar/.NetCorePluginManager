cc68ad2424420cefb5e07cdfa91fc0a561b3897d
k3ldar <s1cart3r@gmail.com>
2019-05-19 08:03:46 +0100

    Initially add non functional Documentation.Plugin to solution

---
+++ Plugins/Documentation.Plugin/Controllers/DocumentationController.cs
@@ -0,0 +1,77 @@
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  .Net Core Plugin Manager is distributed under the GNU General Public License version 3 and  
+ *  is also available under alternative licenses negotiated directly with Simon Carter.  
+ *  If you obtained Service Manager under the GPL, then the GPL applies to all loadable 
+ *  Service Manager modules used on your system as well. The GPL (version 3) is 
+ *  available at https://opensource.org/licenses/GPL-3.0
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  The Original Code was created by Simon Carter (s1cart3r@gmail.com)
+ *
+ *  Copyright (c) 2018 - 2019 Simon Carter.  All Rights Reserved.
+ *
+ *  Product:  Documentation Plugin
+ *  
+ *  File: DocumentationController.cs
+ *
+ *  Purpose:  
+ *
+ *  Date        Name                Reason
+ *  19/05/2019  Simon Carter        Initially Created
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+using System;
+using System.IO;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Shared.Classes;
+using static Shared.Utilities;
+
+using SharedPluginFeatures;
+
+using Middleware;
+using static Middleware.Constants;
+
+namespace DocumentationPlugin.Controllers
+{
+    [LoggedOut]
+    public class DocumentationController : BaseController
+    {
+        #region Private Members
+
+
+        #endregion Private Members
+
+        #region Constructors
+
+        public DocumentationController(ILoginProvider loginProvider, 
+            ISettingsProvider settingsProvider)
+        {
+            if (settingsProvider == null)
+                throw new ArgumentNullException(nameof(settingsProvider));
+
+        }
+
+        #endregion Constructors
+
+        #region Public Action Methods
+
+        [HttpGet]
+        [Breadcrumb(nameof(Languages.LanguageStrings.Login))]
+        public IActionResult Index(string returnUrl)
+        {
+
+            return View();
+        }
+
+        #endregion Public Action Methods
+
+        #region Private Methods
+
+        #endregion Private Methods
+    }
+}
---
+++ Plugins/Documentation.Plugin/Views/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoginPlugin.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
