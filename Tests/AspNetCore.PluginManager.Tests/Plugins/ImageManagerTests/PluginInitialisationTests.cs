72e52dacd3598332551036b974b680109f8f8bf5
k3ldar <s1cart3r@gmail.com>
2021-04-16 08:02:03 +0200

    Add plugin initialise tests, non functional

---
+++ Tests/AspNetCore.PluginManager.Tests/Plugins/ImageManagerTests/PluginInitialisationTests.cs
@@ -0,0 +1,176 @@
+﻿/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  .Net Core Plugin Manager is distributed under the GNU General Public License version 3 and  
+ *  is also available under alternative licenses negotiated directly with Simon Carter.  
+ *  If you obtained Service Manager under the GPL, then the GPL applies to all loadable 
+ *  Service Manager modules used on your system as well. The GPL (version 3) is 
+ *  available at https://opensource.org/licenses/GPL-3.0
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  The Original Code was created by Simon Carter (s1cart3r@gmail.com)
+ *
+ *  Copyright (c) 2018 - 2021 Simon Carter.  All Rights Reserved.
+ *
+ *  Product:  AspNetCore.PluginManager.Tests
+ *  
+ *  File: PluginInitialisationTests.cs
+ *
+ *  Purpose:  Tests for Image Manager Plugin Initialisation
+ *
+ *  Date        Name                Reason
+ *  16/04/2021  Simon Carter        Initially Created
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+using ImageManager.Plugin;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using PluginManager.Abstractions;
+
+using SharedPluginFeatures;
+
+namespace AspNetCore.PluginManager.Tests.Plugins.ImageManagerTests
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class PluginInitialisationTests
+    {
+        private const string GeneralTestsCategory = "Image Manager General Tests";
+
+        [TestMethod]
+        [TestCategory(GeneralTestsCategory)]
+        public void ExtendsIPluginAndIInitialiseEvents()
+        {
+            PluginInitialisation sut = new PluginInitialisation();
+
+            Assert.IsInstanceOfType(sut, typeof(IPlugin));
+            Assert.IsInstanceOfType(sut, typeof(IInitialiseEvents));
+        }
+
+        [TestMethod]
+        [TestCategory(GeneralTestsCategory)]
+        public void GetVersion_ReturnsCurrentVersion_Success()
+        {
+            PluginInitialisation sut = new PluginInitialisation();
+
+            Assert.AreEqual((ushort)1, sut.GetVersion());
+        }
+
+        [TestMethod]
+        [TestCategory(GeneralTestsCategory)]
+        public void Initialize_DoesNotAddItemsToLogger()
+        {
+            PluginInitialisation sut = new PluginInitialisation();
+            TestLogger testLogger = new TestLogger();
+
+            sut.Initialise(testLogger);
+
+            Assert.AreEqual(0, testLogger.Logs.Count);
+        }
+
+        [TestMethod]
+        [TestCategory(GeneralTestsCategory)]
+        public void AfterConfigure_DoesNotConfigurePipeline_Success()
+        {
+            TestApplicationBuilder testApplicationBuilder = new TestApplicationBuilder();
+            PluginInitialisation sut = new PluginInitialisation();
+
+            sut.AfterConfigure(testApplicationBuilder);
+
+            Assert.IsFalse(testApplicationBuilder.UseCalled);
+        }
+
+        [TestMethod]
+        [TestCategory(GeneralTestsCategory)]
+        public void Configure_DoesNotConfigurePipeline_Success()
+        {
+            TestApplicationBuilder testApplicationBuilder = new TestApplicationBuilder();
+            PluginInitialisation sut = new PluginInitialisation();
+
+            sut.Configure(testApplicationBuilder);
+
+            Assert.IsFalse(testApplicationBuilder.UseCalled);
+        }
+
+        [TestMethod]
+        [TestCategory(GeneralTestsCategory)]
+        public void BeforeConfigure_DoesNotRegisterApplicationServices()
+        {
+            TestApplicationBuilder testApplicationBuilder = new TestApplicationBuilder();
+            PluginInitialisation sut = new PluginInitialisation();
+
+            sut.BeforeConfigure(testApplicationBuilder);
+
+            Assert.IsFalse(testApplicationBuilder.UseCalled);
+        }
+
+        [TestMethod]
+        [TestCategory(GeneralTestsCategory)]
+        public void Configure_DoesNotRegisterApplicationServices()
+        {
+            TestApplicationBuilder testApplicationBuilder = new TestApplicationBuilder();
+            PluginInitialisation sut = new PluginInitialisation();
+
+            sut.Configure(testApplicationBuilder);
+
+            Assert.IsFalse(testApplicationBuilder.UseCalled);
+        }
+
+        [TestMethod]
+        [TestCategory(GeneralTestsCategory)]
+        public void Finalise_DoesNotThrowException()
+        {
+            TestApplicationBuilder testApplicationBuilder = new TestApplicationBuilder();
+            PluginInitialisation sut = new PluginInitialisation();
+
+            sut.Finalise();
+        }
+
+        [TestMethod]
+        [TestCategory(GeneralTestsCategory)]
+        public void BeforeConfigureServices_DoesNotThrowException()
+        {
+            TestApplicationBuilder testApplicationBuilder = new TestApplicationBuilder();
+            PluginInitialisation sut = new PluginInitialisation();
+            MockServiceCollection mockServiceCollection = new MockServiceCollection();
+
+            sut.BeforeConfigureServices(mockServiceCollection);
+
+            Assert.AreEqual(0, mockServiceCollection.Count);
+        }
+
+        [TestMethod]
+        [TestCategory(GeneralTestsCategory)]
+        public void ConfigureServices_DoesNotThrowException()
+        {
+            TestApplicationBuilder testApplicationBuilder = new TestApplicationBuilder();
+            PluginInitialisation sut = new PluginInitialisation();
+            MockServiceCollection mockServiceCollection = new MockServiceCollection();
+
+            sut.ConfigureServices(mockServiceCollection);
+
+            Assert.AreEqual(0, mockServiceCollection.Count);
+        }
+
+        [TestMethod]
+        [TestCategory(GeneralTestsCategory)]
+        public void AfterConfigureServices_CreatesContentEditorPolicy_Success()
+        {
+            TestApplicationBuilder testApplicationBuilder = new TestApplicationBuilder();
+            PluginInitialisation sut = new PluginInitialisation();
+            MockServiceCollection mockServiceCollection = new MockServiceCollection();
+
+            sut.AfterConfigureServices(mockServiceCollection);
+
+            Assert.IsTrue(mockServiceCollection.HasServiceRegistered<IDynamicContentProvider>(ServiceLifetime.Singleton));
+
+            string[] claims = { "ManageContent", "StaffMember", "Name", "UserId", "Email" };
+            Assert.IsTrue(mockServiceCollection.HasPolicyConfigured("ContentEditor", claims));
+        }
+    }
+}
