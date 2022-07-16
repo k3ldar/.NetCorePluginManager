788b6322714071e3b85537212bbc51c039355f5e
k3ldar <s1cart3r@gmail.com>
2022-07-16 13:03:23 +0200

    Initial start on license provider

--- DAL/PluginManager.DAL.TextFiles.Tests/PluginInitialisationTests.cs
+++ DAL/PluginManager.DAL.TextFiles.Tests/PluginInitialisationTests.cs
@@ -137,7 +137,7 @@ namespace PluginManager.DAL.TextFiles.Tests
         [TestCategory(GeneralTestsCategory)]
         public void BeforeConfigureServices_DoesNotThrowException()
         {
-            const int RegisteredService = 36;
+            const int RegisteredService = 39;
 
             PluginInitialisation sut = new PluginInitialisation();
             MockServiceCollection mockServiceCollection = new MockServiceCollection();
