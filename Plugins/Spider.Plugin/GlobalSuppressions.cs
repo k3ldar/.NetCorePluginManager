39cb4ed5c461a45ce320e39afa4eb2cf1767b9bb
Si Carter <s1cart3r@gmail.com>
2024-07-07 19:22:34 +0200

    Validate and review CA

--- Plugins/Spider.Plugin/GlobalSuppressions.cs
+++ Plugins/Spider.Plugin/GlobalSuppressions.cs
@@ -6,3 +6,4 @@
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Style", "IDE0220:Add explicit cast", Justification = "the conversion is valid due to above line in retrieving array", Scope = "member", Target = "~M:Spider.Plugin.Classes.Robots.SortAndFilterDenyRoutesByAgent(Microsoft.AspNetCore.Mvc.Infrastructure.IActionDescriptorCollectionProvider,SharedPluginFeatures.IRouteDataService,System.Collections.Generic.List{System.Type})~System.Collections.Generic.Dictionary{System.String,System.Collections.Generic.List{SharedPluginFeatures.IRobotRouteData}}")]
+[assembly: SuppressMessage("Performance", "CA1835:Prefer the 'Memory'-based overloads for 'ReadAsync' and 'WriteAsync'", Justification = "Reviewed and left as is", Scope = "member", Target = "~M:Spider.Plugin.SpiderMiddleware.Invoke(Microsoft.AspNetCore.Http.HttpContext)~System.Threading.Tasks.Task")]
